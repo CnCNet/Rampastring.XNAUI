8b7d7b923ba5378609d7f73e2bfe20fe932e0e61
SadPencil <sadpencil@outlook.com>
2026-05-08 13:51:55 +0800

    Update FontManager.cs

--- FontManagement/FontManager.cs
+++ FontManagement/FontManager.cs
@@ -7,6 +7,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Rampastring.Tools;
+using Rampastring.XNAUI.Extensions;
 
 namespace Rampastring.XNAUI.FontManagement;
 
